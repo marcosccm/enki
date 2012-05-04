d725b89ffd8327b77e333e4b28d80eb959cca76d
marcosccm <marcosccm@gmail.com>
2012-05-03 23:27:32 -0300

    fleshing out the simple solution

--- enki.hs
+++ enki.hs
@@ -1,7 +1,11 @@
 type Noun = String
-data RuleType = All | None | Some
+data RuleType = All | None | Some deriving (Eq)
 data Rule = Rule Noun Noun RuleType
 type Question = Rule
+
+filterRules :: [Rule] -> RuleType -> [Rule]
+filterRules rules rtype = filter (isOfType) rules
+    where isOfType (Rule _ _ t) =  t == rtype
 
 hasRule :: Noun -> Noun -> [Rule] -> Bool
 hasRule x y rules = any (isRule) rules
@@ -9,7 +13,20 @@ hasRule x y rules = any (isRule) rules
 
 makeQuestion :: Question -> [Rule] -> String
 makeQuestion (Rule x y All) xs 
-    | hasRule  x y xs  = "Yes, all " ++ x ++ " are " ++ y
+    | hasRule  x y alls = "Yes, all " ++ x ++ " are " ++ y
+    | otherwise         = "No"
+  where alls = filterRules xs All
+
+makeQuestion (Rule x y None) xs 
+    | hasRule  x y alls   = "No, all " ++ x ++ " are " ++ y
+    | hasRule  x y somes  = "No, some " ++ x ++ " are " ++ y
+    | otherwise        = "Yes, no " ++ x ++ " are " ++ y
+  where alls = filterRules xs All
+        somes = filterRules xs Some
+
+makeQuestion (Rule x y Some) xs 
+    | hasRule  x y somes = "Yes, some " ++ x ++ " are " ++ y
+    | hasRule  x y alls  = "Yes, all " ++ x ++ " are " ++ y
     | otherwise        = "No"
-
-
+  where alls = filterRules xs All
+        somes = filterRules xs Some
